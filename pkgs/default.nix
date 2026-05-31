16a91d9774e2150c3a656ed736b1da55be4c662b
lemonknife <lemonife@protonmail.com>
2026-05-31 09:43:12 -0400

    fix: add breeze-plus to customPkgs

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -3,4 +3,5 @@
 {
   illogical-impulse-oneui4-icons = pkgs.callPackage ./illogical-impulse-oneui4-icons { };
   material-symbols               = pkgs.callPackage ./material-symbols { };
+  breeze-plus                    = pkgs.callPackage ./breeze-plus { };
 }
